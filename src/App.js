80699b7cd6893f23949bfd81c3078ef3cd1fb3c5
johnsonhung906 <khhung906@gmail.com>
2021-11-06 01:16:41 +0800

    router test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
-import React, { Component } from 'react';
-import { HashRouter,Routes, Route, Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useLiff } from 'react-liff';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Game from './pages/Game';
+import Social from './pages/Social';
 
+const App = () => {
+  const [profile, setProfile] = useState('');
+  const { error, liff, isLoggedIn, ready } = useLiff();
 
-const Home = () => <div><h2>Home</h2></div>
-const About = () => <div><h2>About</h2></div>
+  useEffect(() => {
+    if (!isLoggedIn) return;
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter basename="/">
-        <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-          <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          </Routes>
-        </div>
-      </HashRouter>
-    );
-  }
+    (async () => {
+      const profile = await liff.getProfile();
+      setProfile(profile);
+    })();
+  }, [liff, isLoggedIn]);
+
+  return (
+    <HashRouter basename="/">
+      {(error || !isLoggedIn || !ready)? <p>Loading...</p>:
+        <Routes>
+          <Route exact path="/game" component={()=><Game profile={profile} />} />
+          <Route exact path="/social" component={() => <Social profile={profile} />} />
+        </Routes>
+      }
+    </HashRouter>
+  )
 }
 
 export default App;
+
+
+// import React, { Component } from 'react';
+// import { HashRouter,Routes, Route, Link } from "react-router-dom";
+
+
+// const Home = () => <div><h2>Home</h2></div>
+// const About = () => <div><h2>About</h2></div>
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <HashRouter basename="/">
+//         <div>
+//           <ul>
+//             <li><Link to="/">Home</Link></li>
+//             <li><Link to="/about">About</Link></li>
+//           </ul>
+//           <Routes>
+//           <Route exact path="/" component={Home} />
+//           <Route path="/about" component={About} />
+//           </Routes>
+//         </div>
+//       </HashRouter>
+//     );
+//   }
+// }
+
+// export default App;
