bfd23fcefeac9e45451427498e777ee91fb02dd4
johnsonhung906 <khhung906@gmail.com>
2021-11-06 01:13:01 +0800

    router test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { useLiff } from 'react-liff';
-import { HashRouter, Route } from "react-router-dom";
-import Game from './pages/Game';
-import Social from './pages/Social';
+import React, { Component } from 'react';
+import { HashRouter,Routes, Route, Link } from "react-router-dom";
 
-const App = () => {
-  const [profile, setProfile] = useState('');
-  const { error, liff, isLoggedIn, ready } = useLiff();
 
-  useEffect(() => {
-    if (!isLoggedIn) return;
+const Home = () => <div><h2>Home</h2></div>
+const About = () => <div><h2>About</h2></div>
 
-    (async () => {
-      const profile = await liff.getProfile();
-      setProfile(profile);
-    })();
-  }, [liff, isLoggedIn]);
-
-  return (
-    <HashRouter basename="/">
-      {(error || !isLoggedIn || !ready)? <p>Loading...</p>:
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
         <div>
-          <Route exact path="/game" component={()=><Game profile={profile} />} />
-          <Route exact path="/social" component={() => <Social profile={profile} />} />
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+          <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          </Routes>
         </div>
-      }
-    </HashRouter>
-  )
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
